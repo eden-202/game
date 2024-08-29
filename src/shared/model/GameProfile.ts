5f4462a20dc91a49216b6852ebed20d7a2edd762
ChenBallay <chenb100496@gmail.com>
2024-08-29 23:15:30 +0300

    יאללה נעבור לחלק 2

--- src/shared/model/GameProfile.ts
+++ src/shared/model/GameProfile.ts
@@ -1,12 +1,15 @@
 
 export class GameProfile {
-
-    constructor(
-        public id: number,
-        public name : string,
-        public description : string,
-        public url : string
-    ) {
-
+    id: number;
+    name: string;
+    description: string;
+    url: string;
+  
+    constructor(id: number, name: string, description: string, url: string) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.url = url;
     }
-}
+  }
+  
